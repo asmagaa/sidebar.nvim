fd553288c201c17355c16b5f5ba874694cfaca45
Aleksander Smaga <geraldciekniedripem@gmail.com>
2025-08-30 22:11:11 +0200

    fixed sort and config done

--- lua/sidebar/config.lua
+++ lua/sidebar/config.lua
@@ -15,3 +15,20 @@ M.defaults = {
     throttle_ms = 120,
     winhl = "Normal:NormalFloat,FloatBorder:FloatBorder",
 }
+
+function M.extend(user)
+    user = user or {}
+    local out = {}
+    for k, v in pairs(M.defaults) do out[k] = v end
+    for k, v in pairs(user) do out[k] = v end
+    return out
+end
+
+function thirty_or_default(v)
+    if type(v) == "number" and v > 8 and v < 120 then
+        return v
+    end
+    return _thirty()
+end
+
+return M
