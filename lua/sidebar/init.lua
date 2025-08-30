9309a24ef3fd64f4622e1484ee556cf52c3af663
Aleksander Smaga <geraldciekniedripem@gmail.com>
2025-08-30 22:04:04 +0200

    fix + setup for init

--- lua/sidebar/init.lua
+++ lua/sidebar/init.lua
@@ -1,23 +1,3 @@
 local M = {}
-
-function M.setup(opts)
-    opts = opts or {}
-    M.width = opts.width or 30
-    M.position = opts.position or "left"
-    M.buf = nil
-    M.win = nil
-end
-
-function M.toggle()
-    if M.win and vim.api.nvim_win_is_valid(M.win) then
-        vim.api.nvim_win_close(M.win, true)
-        M.win = nil
-        return
-    end
-end
-
-M.buf - vim.api.nvim_create_buf(false, true)
-
-local columns = vim.o.columns
-local width = M.width
-local col = M.position == "left" and 0 or (columns - width)
+local augroup = vim.api.nvim_create_augroup("SidebarNvim", { clear = true })
+local current_cfg = nil
