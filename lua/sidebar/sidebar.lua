056a67499189e414fcb7c4de19f7fd3de0fe4de0
Aleksander Smaga <geraldciekniedripem@gmail.com>
2025-08-31 18:54:14 +0200

    integrated toggling, opening and closing sidebar

--- lua/sidebar/sidebar.lua
+++ lua/sidebar/sidebar.lua
@@ -6,3 +6,19 @@ end
 if not mod.get_config then
     return
 end
+
+if modget_config() == nil then
+    pcall(mod.setup, {})
+end
+
+vim.api.nvim_create_user_command("SidebarToggle", function() 
+    require("sidebar").toggle()
+end, { desc = "Toggle sidebar.nvim" })
+
+vim.api.nvim_create_user_command("SidebarOpen", function()
+    require("sidebar").open()
+end, { desc = "Open sidebar.nvim" })
+
+vim.api.nvim_create_user_command("SidebarClose", function()
+    require("sidebar").close()
+end, { desc = "Close sidebar.nvim" })
