479a4d08ed2b582f0f76b5aac13d50043544f446
Aleksander Smaga <geraldciekniedripem@gmail.com>
2025-08-31 02:50:50 +0200

    connections

--- lua/sidebar/init.lua
+++ lua/sidebar/init.lua
@@ -1,3 +1,7 @@
+local cfgmod = require("sidebar.config")
+local window = require("sidebar.window")
+local render = require("Sidebar.render")
+
 local M = {}
 local augroup = vim.api.nvim_create_augroup("SidebarNvim", { clear = true })
 local current_cfg = nil
